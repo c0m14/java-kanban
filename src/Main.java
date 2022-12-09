b04347a0377728bf4d7b73728c282581dc695de8
Nikita Komochkov <ascii2@yandex.ru>
2022-12-09 10:31:55 +0300

    feat: реализовать класс Task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        Task testTask = new Task("Продумать структуру проекта",
+                "Прежде чем приступить к написанию кода"
+                + " нужно понять какие классы у нас будут и как они будут связаны между собой", 5);
+        System.out.println(testTask);
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Task {
+    protected String name;
+    protected String description;
+    protected int id;
+    protected String status;
+
+    public Task(String name, String description, int id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        status = "NEW";
+    }
+
+    @Override
+    public String toString() {
+        String result = "Task{" +
+                        "name='" + name + '\'';
+        if (description != null) {
+            result = result + ", description.length()='" + description.length() + '\'';
+        } else {
+            result = result + ", description.length()='null'";
+        }
+        result = result + ", id=" + id +
+                            ", status='" + status + '\'' +
+                            '}';
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && name.equals(task.name) && Objects.equals(description, task.description) && status.equals(task.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id, status);
+    }
+}
