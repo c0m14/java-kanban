61a6601aa11182c6ff48e8eee182dc67d21bef7c
Nikita Komochkov <ascii2@yandex.ru>
2022-12-10 18:29:54 +0300

    feat: Реализовать класс Subtask

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Subtask {
+public class Subtask extends Task{
+    int epicId;
+
+    public Subtask(int id, String name, String description, int epicId) {
+        super(id, name, description);
+        this.epicId = epicId;
+    }
+
+    public Subtask(int id, String name, int epicId) {
+        super(id, name);
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'';
+        if (description != null) {
+            result = result + ", description.length()='" + description.length() + '\'';
+        } else {
+            result = result + ", description.length()='null'";
+        }
+        result = result +
+                ", status='" + status + '\'' +
+                ", epicId=" + epicId +
+                '}';
+        return result;
+    }
+
 }
