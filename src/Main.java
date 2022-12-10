8ce75eb03771e0684838b4b8fb22d31444a742fa
Nikita Komochkov <ascii2@yandex.ru>
2022-12-10 12:30:46 +0300

    feat: Добавить тест-кейс на обновление статуса задачи

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
 
-        //тест кейс №1
+        //тест кейс №1: создание задач
         Task testTask1 = new Task(manager.getIdCounter(),
                 "Test task 1 with description",
                 "Some description of " + "the test task 1");
@@ -12,6 +12,17 @@ public class Main {
         Task testTask2 = new Task(manager.getIdCounter(),
                 "Test task 2 without description");
         manager.createTask(testTask2);
+
+        for (Task task : manager.getAllTasks()) {
+            System.out.println(task);
+        }
+
+        //тест кейс №2: изменение статуса задач
+        testTask1.setStatus("IN_PROGRESS");
+        manager.updateTask(testTask1, testTask1.getId());
+
+        testTask2.setStatus("SOME_WRONG_STATUS");
+        manager.updateTask(testTask2, testTask2.getId());
 
         for (Task task : manager.getAllTasks()) {
             System.out.println(task);
