630b4a365446aad4a759998ca773fad60b5f0ad0
Nikita Komochkov <ascii2@yandex.ru>
2022-12-12 10:54:26 +0300

    style: Обернуть if в фигурные скобки в Override метода equals

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,6 +1,6 @@
 import java.util.Objects;
 
-public class Subtask extends Task{
+public class Subtask extends Task {
     private int epicId;
 
     public Subtask(int id, String name) {
@@ -38,9 +38,15 @@ public class Subtask extends Task{
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
         Subtask subtask = (Subtask) o;
         return epicId == subtask.epicId;
     }
