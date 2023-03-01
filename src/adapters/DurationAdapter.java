2bdf3d60498e43a6d9328ca3ef25cbdcc9f34352
Nikita Komochkov <ascii2@yandex.ru>
2023-03-01 12:03:58 +0300

    feat: Добавить json адаптеры для LocalDateTime и Duration

---
+++ src/adapters/DurationAdapter.java
@@ -0,0 +1,33 @@
+package adapters;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DurationAdapter extends TypeAdapter<Duration> {
+
+    @Override
+    public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
+        if (duration == null) {
+            jsonWriter.nullValue();
+        } else {
+            jsonWriter.value(duration.toString());
+        }
+    }
+
+    @Override
+    public Duration read(JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return null;
+        } else {
+            return Duration.parse(jsonReader.nextString());
+        }
+    }
+}
--- src/adapters/LocalDateTimeAdapter.java
+++ src/adapters/LocalDateTimeAdapter.java
@@ -1,4 +1,4 @@
-package api;
+package adapters;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
