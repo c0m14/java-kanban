b2be99f4be7aa20117c58d11540240aba87f8648
Nikita Komochkov <ascii2@yandex.ru>
2023-03-06 22:00:39 +0300

    refactor: Убрать переопределение метода equals

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -89,21 +89,6 @@ public class Subtask extends Task {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-        Subtask subtask = (Subtask) o;
-        return epicId == subtask.epicId;
-    }
-
-    @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), epicId);
     }
